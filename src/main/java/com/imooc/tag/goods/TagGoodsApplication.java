6b13bcff4e1e3fc3ae46fdfd038a71914b9f0a1e
wanghg <wanghg@thfund.com.cn>
2023-11-14 16:57:21 +0800

    feat: 初始化

---
+++ src/main/java/com/imooc/tag/goods/TagGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.tag.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagGoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imooc/tag/goods/TagGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.tag.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
